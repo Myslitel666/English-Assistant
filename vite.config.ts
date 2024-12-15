4f9e59a835123e705a8f8ad04eabc1cbb56aeb82
Myslitel666 <tretyakov_02@bk.ru>
2024-12-15 18:53:37 +0500

    trash is deleted.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
+	server: {
+        port: 5175, // Укажите другой порт
+    },
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
