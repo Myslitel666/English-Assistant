0e84946c4389aa10ea3e46b87c830a06c2aeaf07
Myslitel666 <tretyakov_02@bk.ru>
2024-12-15 19:05:52 +0500

    Header has been added.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,43 @@
 <script lang="ts">
-	import '../app.css';
-	import { ThemeProvider } from 'svelte-elegant';
+	import '../app.css'
+	import { 
+		ThemeProvider, 
+		Header,
+		ColorThemeSwitch
+	} from "svelte-elegant";
+	import { themeMode } from 'svelte-elegant/stores/themeStore';
+	import type { IColorThemeStore } from "svelte-elegant/interfaces";
+	import { themeStore } from "svelte-elegant/stores/themeStore";
+
+	let theme: IColorThemeStore | undefined;
+
+	// Подписываемся на изменения темы
+	themeStore.subscribe(value => {
+		theme = value; //Инициализация объекта темы
+	});
 
 	let { children } = $props();
 </script>
+
+<Header height = 3.5rem>
+	<button style:gap = '0.5rem'>
+		<img src = {$themeMode === 'light' ? '/Images/Logo.svg' : '/Images/Logo.svg'} alt = 'logo' class = 'logo' />
+		<p style:font-size=26px>
+			<span 
+				style:color = {theme?.colors.primary}
+				style:transition = 'all 0.3s'
+			>
+				English Assistant
+			</span> 
+		</p>
+	</button>
+	<div 
+		style:margin-left = auto
+		style:margin-right = 1.25rem
+	>
+		<ColorThemeSwitch />
+	</div>
+</Header>
 
 <ThemeProvider>
 	<div class="app">
@@ -12,3 +46,10 @@
 		</main>
 	</div>
 </ThemeProvider>
+
+<style>
+	.logo {
+		width: 3.25rem;
+		height: 3.25rem;
+	}
+</style>
