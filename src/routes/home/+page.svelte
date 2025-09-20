09ffca5c7dd75f37012748dee814fb812752bb3e
Myslitel666 <tretyakov_02@bk.ru>
2025-09-20 23:18:42 +0500

    i

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -48,7 +48,7 @@
     <AutoComplete bind:value={subjectWord} options={subjects} label="Subject" />
   </div>
   <div class="subject">
-    <DataGrid width="902px" {columns} rows={specialWords} />
+    <DataGrid width="100%" maxWidth="902px" {columns} rows={specialWords} />
   </div>
 </div>
 
